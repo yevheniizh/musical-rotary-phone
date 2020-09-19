3a08056c34fec3de6d2d4a7abe621ad10cdf695f
Yevhenii <zh.zhyrov@gmail.com>
2020-09-19 18:16:57 +0300

    some changes of 'console.log' outputs;add jQuery equal code;

--- other/learn.js
+++ other/learn.js
@@ -1,65 +1,80 @@
-for (var i = 0; i < 100; i++) {
-    console.log(i);
-}
-
-//key-description examples
-// var cat = {
-//     name: 'Snizhok',
-//     color: 'white'
+// for (var i = 0; i < 100; i++) {
+//     console.log(i);
 // }
 
-// var cat1 = {
-//     name: 'Murzik',
-//     color: 'black'
+// //key-description examples
+// // var cat = {
+// //     name: 'Snizhok',
+// //     color: 'white'
+// // }
+
+// // var cat1 = {
+// //     name: 'Murzik',
+// //     color: 'black'
+// // }
+
+
+// //constructor example
+// function Cat (name, color) {
+//     this.name = name;
+//     this.color = color;
+
+//     this.run = function() {
+//         console.log(this.name + ' runs!');
+//     }
+//     return this;
 // }
 
+// var cat1 = new Cat('Snizhok', 'white');
+// var cat2 = new Cat('Murzik', 'black');
 
-//constructor example
-function Cat (name, color) {
-    this.name = name;
-    this.color = color;
-
-    this.run = function() {
-        console.log(this.name + ' runs!');
-    }
-    return this;
-}
-
-var cat1 = new Cat('Snizhok', 'white');
-var cat2 = new Cat('Murzik', 'black');
-
-console.log(cat1.name);
-console.log(cat2.name);
-cat1.run();
-cat2.run();
+// console.log(cat1.name);
+// console.log(cat2.name);
+// cat1.run();
+// cat2.run();
 
 
-//delaying function
-setTimeout(function() {
-    cat1.run();  
-}, 2000)
+// //delaying function
+// setTimeout(function() {
+//     cat1.run();  
+// }, 2000)
 
 
-//using onevent attribute:
-//method1 (in HTML inside tag with onclick-property)
-//method2 (by id (#) inside tag)
-function clicker() {
-    cat1.name();
-}
+// //using onevent attribute:
+// //method1 (in HTML inside tag with onclick-property)
+// //method2 (by id (#) inside tag)
+// function clicker() {
+//     cat1.name();
+// }
 
-function clicker2() {
-    cat2.name();
-}
+// function clicker2() {
+//     cat2.name();
+// }
 
-clickbtn.onclick = clicker;
-clickbtn.onclick = clicker2; //здесь будет вызываться только последняя функция
+// clickbtn.onclick = clicker;
+// clickbtn.onclick = clicker2; //здесь будет вызываться только последняя функция
 
-//method3
-clickbtn.onclick = clicker;
-eventbtn.addEventListener('click', function(e) { //eventbtn - id inside some tag
-    console.log('text'); 
-    console.log (e);                      //ВЫЗЫВАЕТ В КОНСОЛЬ
-}, false);
+// //method3
+// clickbtn.onclick = clicker;
+// eventbtn.addEventListener('click', function(e) { //eventbtn - id inside some tag
+//     console.log('text'); 
+//     console.log (e);                      //ВЫЗЫВАЕТ В КОНСОЛЬ
+// }, false);
 
-eventbtn.addEventListener('click', clicker, false); //+ЕЩЁ ВЫЗЫВАЕТ ALERT
-// enentbtn.removeEventListener('click', clicker)
+// eventbtn.addEventListener('click', clicker, false); //+ЕЩЁ ВЫЗЫВАЕТ ALERT
+// // enentbtn.removeEventListener('click', clicker)
+
+
+
+//Promise
+let xxx = new Promise ( (resolve, reject) => {
+    setTimeout( () => {
+        if (Math.random() > 0.5) {
+            resolve("Success!");
+        } else {
+            reject("Error!");
+        }
+    }, 1000);
+})
+xxx.then((data) => console.info(data))
+.catch((data) => console.error(data));
--- other/tips/tips.js
+++ other/tips/tips.js
@@ -1,201 +1,209 @@
 // Поле "Bill"
 
-// let bill = document.getElementById('InitialInputs__bill'); //берем элемент инпута
-// bill.addEventListener('focusout', function() {  //вызываем событие, при котором после ухода с зоны инпута обновляется значение с округлением до 0.00
-//     tip_total_count();
-//     bill_total_count();
-//     let bill_parse = bill.value;
-//     bill_parse = Math.round(bill_parse * 100) / 100; //чомусь не работает
-//     bill_parse = bill_parse.toFixed(2);
-//     return bill.value = bill_parse;
-// }, false);
+let bill = document.getElementById('InitialInputs__bill');
 
-// //Поле "Tip%"
-
-// let tip = document.getElementById('InitialInputs__tip_result'); //берем элемент инпута
-// console.log(tip.value);
-
-// let tip_plus = document.getElementById('InitialInputs__tip_plus'); //берем элемент кнопки "+"
-// console.log(InitialInputs__tip_plus);
-
-// let tip_minus = document.getElementById('InitialInputs__tip_minus'); //берем элемент кнопки "-"
-// console.log(InitialInputs__tip_minus);
-
-// tip_plus.addEventListener('click', function() { //присваиваем событие при нажатии на кнопку "+"
-//     let result = parseInt(tip.value, 10); //берем значение в поле input и преобразовываем со строчного в цифру
-//     if (tip.value <= 0) { //цикл-условие при значениях <0
-//         tip.value = 1;
-//     } else {
-//         tip.value = result + 1;
-//     }
-//     console.log(tip.value);
-//     tip_total_count();
-//     bill_total_count();
-// }, false);
-
-// tip_minus.addEventListener('click', function() { //присваиваем событие при нажатии на кнопку "-"
-//     let result = parseInt(tip.value, 10); //берем значение в поле input и преобразовываем со строчного в цифру
-//     if (tip.value <= 0) { //цикл-условие при значениях <0
-//         tip.value = 0;
-//     } else {
-//         tip.value = result - 1;
-//     }
-//     console.log(tip.value);
-//     tip_total_count();
-//     bill_total_count();
-// }, false);
-
-// //Поле "Number of people" по аналогии
-
-// let NumOfPeople = document.getElementById('InitialInputs__NumOfPeople_result');
-// console.log(NumOfPeople.value);
-
-// let NumOfPeople_plus = document.getElementById('InitialInputs__NumOfPeople_plus');
-// console.log(NumOfPeople_plus.value);
-
-// let NumOfPeople_minus = document.getElementById('InitialInputs__NumOfPeople_minus');
-// console.log(NumOfPeople_minus.value);
-
-// NumOfPeople_plus.addEventListener('click', function() {
-//     let result = parseInt(NumOfPeople.value, 10);
-//     NumOfPeople.value = result + 1; 
-//     console.log(NumOfPeople.value);
-//     tip_total_count();
-//     bill_total_count();
-// }, false);
-
-// NumOfPeople_minus.addEventListener('click', function() {
-//     let result = parseInt(NumOfPeople.value, 10);
-//     if (NumOfPeople.value <= 1) { //цикл-условие при значениях <0
-//         NumOfPeople.value = 1;
-//     } else {
-//         NumOfPeople.value = result - 1;
-//     }; 
-//     console.log(NumOfPeople.value);
-//     tip_total_count();
-//     bill_total_count();
-// }, false);
-
-// //Поле результатов "Tip"
-
-// function tip_total_count() { //вынес общую функцию калькуляции чаевых
-//     let tip_total = (bill.value * (tip.value)/100) / NumOfPeople.value;
-//         tip_total = Math.round(tip_total * 100) / 100;
-//         tip_total = tip_total.toFixed(2);
-//     return document.getElementById('DisplayCount_tip').innerHTML = "$" + tip_total;
-// }
-
-// //Поле результатов "Total"
-
-// function bill_total_count() { //вынес общую функцию калькуляции суммарного счета (с чаевыми)
-//     let bill_total = parseFloat(bill.value, 10) + (parseFloat(bill.value, 10) * (tip.value)/100);
-//         bill_total = Math.round(bill_total * 100) / 100;
-//         bill_total = bill_total.toFixed(2);
-//         console.log(parseFloat(bill.value, 10) + (parseFloat(bill.value, 10) * (tip.value)/100));
-//     return document.getElementById('DisplayCount_total').innerHTML = "$" + bill_total;
-// }
-
-
-//-jQuery-//
-
-
-let bill_jq = $('#InitialInputs__bill'); //берем элемент инпута
-console.log(bill_jq.val());
-
-$(function() {
-    $('bill_jq').on('focusout',function() {
-    tip_total_count_jq();
-    bill_total_count_jq();
-    let bill_jq_parse = bill_jq.val();
-    bill_jq_parse = Math.round(bill_jq_parse * 100) / 100; 
-    bill_jq_parse = bill_jq_parse.toFixed(2);   
-    console.log(bill_jq_parse);
-    return bill_jq.val() = bill_jq_parse;
-    });
-});
+bill.addEventListener('focusout', function() {
+    tip_total_count();
+    bill_total_count();
+    let bill_parse = bill.value;
+    bill_parse = Math.round(bill_parse * 100) / 100; //чомусь не работает
+    bill_parse = bill_parse.toFixed(2);
+    return bill.value = bill_parse;
+}, false);
 
 //Поле "Tip%"
 
-let tip_jq = $('#InitialInputs__tip_result'); //берем элемент инпута
-console.log(tip_jq.val());
+let tip = document.getElementById('InitialInputs__tip_result');
 
-let tip_jq_plus = $('#InitialInputs__tip_plus'); //берем элемент кнопки "+"
-console.log(tip_jq_plus.val());
+let tip_plus = document.getElementById('InitialInputs__tip_plus');
 
-let tip_jq_minus = $('#InitialInputs__tip_minus'); //берем элемент кнопки "-"
-console.log(tip_jq_minus.val());
+let tip_minus = document.getElementById('InitialInputs__tip_minus');
 
-$('tip_jq_plus').click(function() { //присваиваем событие при нажатии на кнопку "+"
-    let result_jq = parseInt(tip_jq.val(), 10); //берем значение в поле input и преобразовываем со строчного в цифру
-    if (tip_jq.val() <= 0) { //цикл-условие при значениях <0
-        tip_jq.val() = 1;
+//присваиваем событие при нажатии на кнопку "+"
+
+tip_plus.addEventListener('click', function() {
+    let result = parseInt(tip.value, 10);
+    if (tip.value <= 0) {
+        tip.value = 1;
     } else {
-        tip_jq.val() = result_jq + 1;
+        tip.value = result + 1;
     }
-    console.log(tip_jq.val());
-    tip_total_count_jq();
-    bill_total_count_jq();
+    tip_total_count();
+    bill_total_count();
 }, false);
 
-$('tip_jq_minus').click(function() { //присваиваем событие при нажатии на кнопку "-"
-    let result_jq = parseInt(tip_jq.val(), 10); //берем значение в поле input и преобразовываем со строчного в цифру
-    if (tip_jq.val() <= 0) { //цикл-условие при значениях <0
-        tip_jq.val() = 0;
+//присваиваем событие при нажатии на кнопку "-"
+
+tip_minus.addEventListener('click', function() {
+    let result = parseInt(tip.value, 10); //берем значение в поле input и преобразовываем со строчного в цифру
+    if (tip.value <= 0) { //цикл-условие при значениях <0
+        tip.value = 0;
     } else {
-        tip_jq.val() = result_jq - 1;
+        tip.value = result - 1;
     }
-    console.log(tip_jq.val());
-    tip_total_count_jq();
-    bill_total_count_jq();
+    tip_total_count();
+    bill_total_count();
 }, false);
 
-//Поле "Number of people" по аналогии
+//Поле "Number of people"
 
-let NumOfPeople_jq = $('#InitialInputs__NumOfPeople_result');
-console.log(NumOfPeople_jq.val());
+let NumOfPeople = document.getElementById('InitialInputs__NumOfPeople_result');
 
-let NumOfPeople_plus_jq = $('#InitialInputs__NumOfPeople_plus');
-console.log(NumOfPeople_plus_jq.val());
+let NumOfPeople_plus = document.getElementById('InitialInputs__NumOfPeople_plus');
 
-let NumOfPeople_minus_jq = $('#InitialInputs__NumOfPeople_minus');
-console.log(NumOfPeople_minus_jq.val());
+let NumOfPeople_minus = document.getElementById('InitialInputs__NumOfPeople_minus');
 
-$('NumOfPeople_plus_jq').click(function() {
-    let result_jq = parseInt(NumOfPeople_jq.val(), 10);
-    NumOfPeople_jq.val() = result_jq + 1; 
-    console.log(NumOfPeople_jq.val());
-    tip_total_count_jq();
-    bill_total_count_jq();
+//присваиваем событие при нажатии на кнопку "+"
+
+NumOfPeople_plus.addEventListener('click', function() {
+    let result = parseInt(NumOfPeople.value, 10);
+    NumOfPeople.value = result + 1;
+    tip_total_count();
+    bill_total_count();
 }, false);
 
-$('#NumOfPeople_minus').click(function() {
-    let result_jq = parseInt(NumOfPeople_jq.val(), 10);
-    if (NumOfPeople_jq.val() <= 1) { //цикл-условие при значениях <0
-        NumOfPeople_jq.val() = 1;
+//присваиваем событие при нажатии на кнопку "-"
+
+NumOfPeople_minus.addEventListener('click', function() {
+    let result = parseInt(NumOfPeople.value, 10);
+    if (NumOfPeople.value <= 1) {
+        NumOfPeople.value = 1;
     } else {
-        NumOfPeople_jq.val() = result_jq - 1;
-    }; 
-    console.log(NumOfPeople_jq.val());
-    $(tip_total_count_jq());
-    $(bill_total_count_jq());
-    console.log( $(bill_total_count_jq()));
+        NumOfPeople.value = result - 1;
+    };
+    tip_total_count();
+    bill_total_count();
 }, false);
 
 //Поле результатов "Tip"
 
-$(function tip_total_count_jq() { //вынес общую функцию калькуляции чаевых
-    let tip_total_jq = (bill_jq.val() * (tip_jq.val())/100) / NumOfPeople_jq.val();
-        tip_total_jq = Math.round(tip_total_jq * 100) / 100;
-        tip_total_jq = tip_total_jq.toFixed(2);
-    return $('#DisplayCount_tip').val("$" + tip_total_jq);
-});
+function tip_total_count() {
+    let tip_total = (bill.value * (tip.value)/100) / NumOfPeople.value;
+        tip_total = Math.round(tip_total * 100) / 100;
+        tip_total = tip_total.toFixed(2);
+        console.log('Tip = $' + tip_total);
+    return document.getElementById('DisplayCount_tip').innerHTML = "$" + tip_total;
+}
 
 //Поле результатов "Total"
 
-$(function bill_total_count_jq() { //вынес общую функцию калькуляции суммарного счета (с чаевыми)
-    let bill_total_jq = parseFloat(bill_jq.val(), 10) + (parseFloat(bill_jq.val(), 10) * (tip_jq.val())/100);
-        bill_total_jq = Math.round(bill_total_jq * 100) / 100;
-        bill_total_jq = bill_total_jq.toFixed(2);
-        console.log(bill_total_jq);
-    return $('#DisplayCount_total').val("$" + bill_total_jq);
-});
+function bill_total_count() {
+    let bill_total = parseFloat(bill.value, 10) + (parseFloat(bill.value, 10) * (tip.value)/100);
+        bill_total = Math.round(bill_total * 100) / 100;
+        bill_total = bill_total.toFixed(2);
+        console.log('Total = $' + bill_total);
+    return document.getElementById('DisplayCount_total').innerHTML = "$" + bill_total;
+}
+
+
+
+
+// -jQuery-//
+
+
+
+
+// let bill_jq = $('#InitialInputs__bill');
+
+// $(function() { 
+//     bill_jq.focusout(() => {
+//     tip_total_count_jq();
+//     bill_total_count_jq();
+//     let bill_jq_parse = bill_jq.val();
+//     bill_jq_parse = Math.round(bill_jq_parse * 100) / 100; 
+//     bill_jq_parse = bill_jq_parse.toFixed(2);
+//     return bill_jq.val(bill_jq_parse);
+//     });
+// });
+
+// //Поле "Tip%"
+
+// let tip_jq = $('#InitialInputs__tip_result');
+
+// let tip_jq_plus = $('#InitialInputs__tip_plus');
+
+// let tip_jq_minus = $('#InitialInputs__tip_minus');
+
+// //присваиваем событие при нажатии на кнопку "+"
+
+// tip_jq_plus.click(() => { 
+//     let result_jq = parseInt(tip_jq.val(), 10)
+//     if (result_jq <= 0) {
+//         result_jq = tip_jq.val(1);
+//     } else {
+//         let result_jq = tip_jq.val();
+//         result_jq = tip_jq.val(++result_jq);
+//     }
+//     tip_total_count_jq();
+//     bill_total_count_jq();
+// });
+
+// //присваиваем событие при нажатии на кнопку "-"
+
+// tip_jq_minus.click(() => { 
+//     let result_jq = parseInt(tip_jq.val(), 10)
+//     if (result_jq <= 0) {
+//         result_jq = tip_jq.val(0);
+//     } else {
+//         let result_jq = tip_jq.val();
+//         result_jq = tip_jq.val(--result_jq);
+//     }
+//     tip_total_count_jq();
+//     bill_total_count_jq();
+// });
+
+// //Поле "Number of people"
+
+// let NumOfPeople_jq = $('#InitialInputs__NumOfPeople_result');
+
+// let NumOfPeople_plus_jq = $('#InitialInputs__NumOfPeople_plus');
+
+// let NumOfPeople_minus_jq = $('#InitialInputs__NumOfPeople_minus');
+
+// //присваиваем событие при нажатии на кнопку "+"
+
+// NumOfPeople_plus_jq.click(() => {
+//     let result_jq = parseInt(NumOfPeople_jq.val(), 10);
+//     if (result_jq < 1) {
+//         result_jq = NumOfPeople_jq.val(1);
+//     } else {
+//         let result_jq = NumOfPeople_jq.val();
+//         result_jq = NumOfPeople_jq.val(++result_jq); 
+//     }
+//     tip_total_count_jq();
+//     bill_total_count_jq();
+// });
+
+// //присваиваем событие при нажатии на кнопку "-"
+
+// NumOfPeople_minus_jq.click(() => {
+//     let result_jq = parseInt(NumOfPeople_jq.val(), 10);
+//     if (result_jq <= 1) {
+//         result_jq = NumOfPeople_jq.val(1);
+//     } else {
+//         let result_jq = NumOfPeople_jq.val();
+//         result_jq = NumOfPeople_jq.val(--result_jq);
+//     }
+//     tip_total_count_jq();
+//     bill_total_count_jq();
+// });
+
+// //Поле результатов "Tip"
+
+// tip_total_count_jq = () => { //вынес общую функцию калькуляции чаевых
+//     let tip_total_jq = (bill_jq.val() * (tip_jq.val())/100) / NumOfPeople_jq.val();
+//         tip_total_jq = Math.round(tip_total_jq * 100) / 100;
+//         tip_total_jq = tip_total_jq.toFixed(2);
+//         console.log('Tip = $' + tip_total_jq);
+//     return bill_total_jq = $('#DisplayCount_tip').html("$" + tip_total_jq);
+// }
+
+// // //Поле результатов "Total"
+
+// bill_total_count_jq = () => { //вынес общую функцию калькуляции суммарного счета (с чаевыми)
+//     let bill_total_jq = parseFloat(bill_jq.val(), 10) + (parseFloat(bill_jq.val(), 10) * (tip_jq.val())/100);
+//         bill_total_jq = Math.round(bill_total_jq * 100) / 100;
+//         bill_total_jq = bill_total_jq.toFixed(2);
+//         console.log('Total = $' + bill_total_jq);
+//     return bill_total_jq = $('#DisplayCount_total').html("$" + bill_total_jq);
+// }
